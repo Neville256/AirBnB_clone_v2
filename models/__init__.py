5731f0bb4a2db0963ad3ef65c7c60a9ec6579816
Neville256 <nsoundy796@gmail.com>
2023-09-19 12:51:27 -0700

    amenity.py

--- models/__init__.py
+++ models/__init__.py
@@ -2,6 +2,13 @@
 """This module instantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
 
+import os
 
-storage = FileStorage()
+from models.engine.db_storage import DBStorage
+from models.engine.file_storage import FileStorage
+
+storage = DBStorage() if os.getenv(
+    'HBNB_TYPE_STORAGE') == 'db' else FileStorage()
+"""A unique FileStorage/DBStorage instance for all models.
+"""
 storage.reload()
