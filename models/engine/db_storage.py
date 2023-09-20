0de68190d8a589f25e319197e8163da6ee70e9ad
Anzelah <jepkoechanzellah@gmail.com>
2023-09-20 15:53:36 -0700

    remove models from db storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """This module defines a class to manage database storage for hbnb clone"""
 import os
-import models
+#import models
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 
