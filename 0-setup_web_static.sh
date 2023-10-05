29db47f478f4d2beffe9e1ac0fcb853db5a0fc4f
Neville256 <nsoundy796@gmail.com>
2023-10-05 11:56:15 -0700

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 # Sets up a web server for deployment of web_static.
 
-apt-get update
-apt-get install -y nginx
+sudo apt-get update
+sudo apt-get install -y nginx
 
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "Holberton School" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+sudo echo "Holberton School" > /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-chown -R ubuntu:nginx /data/
-chmod -R 755 /data/web_static/
+sudo chown -R ubuntu:nginx /data/
+sudo chmod -R 755 /data/web_static/
 
 printf %s "server {
     listen 80 default_server;
@@ -35,7 +35,7 @@ printf %s "server {
 }" > /etc/nginx/sites-available/my_config
 
 # Create a symbolic link to enable the configuration
-ln -s /etc/nginx/sites-available/my_config /etc/nginx/sites-enabled/
+sudo ln -s /etc/nginx/sites-available/my_config /etc/nginx/sites-enabled/
 
 # Restart Nginx to apply the configuration
 service nginx restart
