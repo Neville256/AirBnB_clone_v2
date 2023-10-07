7c1a6440a857c775ad25d09cc1785f6dad3a2e31
Neville256 <nsoundy796@gmail.com>
2023-10-07 02:22:17 -0700

    0-setup_web_static.sh

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
 # Fabfile to distribute an archive to a web server.
 
-
 import os.path
-from fabric.api import env
-from fabric.api import put
-from fabric.api import run
+from fabric.api import env, put, run
 
 env.hosts = ["54.87.172.124", "54.84.66.24"]
-
 
 def do_deploy(archive_path):
     """Distributes an archive to a web server.
@@ -19,22 +15,33 @@ def do_deploy(archive_path):
         If the file doesn't exist at archive_path or an error occurs - False.
         Otherwise - True.
     """
-    if os.path.isfile(archive_path) is False:
+    if not os.path.isfile(archive_path):
         return False
-    file = archive_path.split("/")[-1]
-    name = file.split(".")[0]
 
-    if put(archive_path, "/tmp/{}".format(file)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
-    if run("mkdir -p /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-           format(file, name)).failed is True:
-        return False
-    if run("rm /tmp/{}".format(file)).failed is True:
-        return False
-    if run("mv /data/web_static/releases/{}/web_static/* "
+    # Extract the filename and name without extension
+    file_name = os.path.basename(archive_path)
+    name = os.path.splitext(file_name)[0]
+
+    # Upload the archive to /tmp
+    put(archive_path, "/tmp/{}".format(file_name))
+
+    # Create the necessary directory structure
+    run("mkdir -p /data/web_static/releases/{}/".format(name))
+
+    # Unpack the archive
+    run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(file_name, name))
+
+    # Delete the uploaded archive
+    run("rm /tmp/{}".format(file_name))
+
+    # Move contents to the proper location
+    run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(name, name))
+
+    # Remove the old symlink
+    run("rm -rf /data/web_static/current")
+
+    # Create a new symlink
+    run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(name))
+
+    print("New version deployed!")
+    return True
