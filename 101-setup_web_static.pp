3e4ac2335776e6860e359e8f07b13ab74616b3f8
Neville256 <nsoundy796@gmail.com>
2023-10-07 02:49:42 -0700

    0-setup_web_static.sh

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,54 +1,50 @@
- Install Nginx
-package { 'nginx':
-  ensure => 'installed',
-}
+# Define the class
+class your_module::web_static_config {
+  # Install Nginx package
+  package { 'nginx':
+    ensure => 'installed',
+  }
 
-# Create directories
-file { '/data/web_static/shared':
-  ensure => 'directory',
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
-}
+  # Create directories
+  file { '/data/web_static/shared':
+    ensure => 'directory',
+    owner  => 'ubuntu',
+    group  => 'ubuntu',
+  }
 
-file { '/data/web_static/releases/test':
-  ensure => 'directory',
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
-}
+  file { '/data/web_static/releases/test':
+    ensure => 'directory',
+    owner  => 'ubuntu',
+    group  => 'ubuntu',
+  }
 
-# Create index.html file
-file { '/data/web_static/releases/test/index.html':
-  ensure  => 'file',
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  content => 'Holberton School',
-}
+  # Create index.html file
+  file { '/data/web_static/releases/test/index.html':
+    ensure  => 'file',
+    owner   => 'ubuntu',
+    group   => 'ubuntu',
+    content => 'Holberton School',
+  }
 
-# Remove the existing symbolic link if it exists
-file { '/data/web_static/current':
-  ensure => 'absent',
-}
+  # Remove the existing symbolic link if it exists
+  file { '/data/web_static/current':
+    ensure => 'absent',
+  }
 
-# Create a symbolic link
-file { '/data/web_static/current':
-  ensure => 'link',
-  target => '/data/web_static/releases/test',
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
-}
+  # Create a symbolic link
+  file { '/data/web_static/current':
+    ensure => 'link',
+    target => '/data/web_static/releases/test',
+    owner  => 'ubuntu',
+    group  => 'ubuntu',
+  }
 
-# Configure Nginx
-file { '/etc/nginx/sites-available/default':
-  ensure  => 'file',
-  owner   => 'root',
-  group   => 'root',
-  content => template('your_module/nginx_config.erb'), # Use an ERB template for your Nginx config
-  require => Package['nginx'], # Ensure Nginx is installed before applying this config
-}
-
-# Restart Nginx service
-service { 'nginx':
-  ensure    => 'running',
-  enable    => true,
-  require   => File['/etc/nginx/sites-available/default'],
+  # Configure Nginx
+  file { '/etc/nginx/sites-available/default':
+    ensure  => 'file',
+    owner   => 'root',
+    group   => 'root',
+    source  => 'puppet:///modules/your_module/nginx_config.erb',
+    require => Package['nginx'], # Ensure Nginx is installed before applying this config
+  }
 }
